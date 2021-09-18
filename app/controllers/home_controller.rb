7cb8000b4c81dc0a22eaed5102533b2ff81ea978
MikolaPy <patriotsssr19@gmail.com>
2021-09-19 00:10:14 +0300

    pass data to vue
    Signed-off-by: MikolaPy <patriotsssr19@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @global_store = { global_store: [1,2,3]}
   end
 end
--- app/javascript/src/components/HelloVue.vue
+++ app/javascript/src/components/HelloVue.vue
@@ -1,28 +1,27 @@
 <template id="">
 <p>
- {{ mes }}
-</p>
-<p>
- {{ mes2 }}
 </p>
 
 </template>
 <script>
 import { ref } from 'vue'
 
+
 export default { 
 	name: 'HelloVue',
 	setup() { 
 		const mes = ref('Word');
-		console.log(mes);
 		return { 
 			mes
 		}
 	},
 	data() {
 		return {
-			mes2: 'mes2'
+			global_store: document.getElementById('global_store')
 		};
+	},
+	created() { 
+		console.log(global_store.dataset.globalStore);
 	}
 }
 </script>
